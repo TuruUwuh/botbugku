d4d9a92ad852cb74078aa5e7335377105ac4e65d
ShinChan Senpai <119733065+TuruUwuh@users.noreply.github.com>
2023-11-06 11:15:10 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -646,7 +646,7 @@ conn.readMessages([m.key])
 }
 }
 // itunya
-      if (!conn.self) {
+      if (!conn.public) {
          if (!m.key.fromMe && !isCreator) return
       }
       if (m.mtype == 'viewOnceMessage' && m.msg.viewOnce) {
