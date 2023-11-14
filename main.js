8577d320a40a2829abaeee14a7537ea4c5294812
ShinChan Senpai <119733065+TuruUwuh@users.noreply.github.com>
2023-11-14 20:55:06 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -634,48 +634,6 @@ async function tryServer1(url) {
     let response = await axios.get(tiklydownAPI);
     return response.data;
 }
-
-async function tryServer2(url) {
-    // jika server 1 gagal, gunakan tikdown.org/id
-    const tikdownURL = 'https://tikdown.org/id';
-    const response = await axios.get(tikdownURL);
-
-    if (response.status === 200) {
-        const gettoken = await axios.get("https://tikdown.org/id");
-        const cheerio = require('cheerio');
-        const $ = cheerio.load(gettoken.data);
-        const token = $("#download-form > input[type=hidden]:nth-child(2)").attr(
-            "value"
-        );
-        const param = {
-            url: url,
-            _token: token,
-        };
-        const {
-            data
-        } = await axios.request("https://tikdown.org/getAjax?", {
-            method: "post",
-            data: new URLSearchParams(Object.entries(param)),
-            headers: {
-                "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-                "user-agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36",
-            },
-        });
-        var getdata = cheerio.load(data.html);
-        if (data.status) {
-            return {
-                status: true,
-                thumbnail: getdata("img").attr("src"),
-                video: getdata("div.download-links > div:nth-child(1) > a").attr("href"),
-                audio: getdata("div.download-links > div:nth-child(2) > a").attr("href"),
-            };
-        } else
-            return {
-                status: false,
-            };
-    };
-}
-
 //NHENTAI SCRAPE TEST
 async function nhentaiScraper(id) {
 	let uri = id ? `https://cin.guru/v/${+id}/` : 'https://cin.guru/'
@@ -1745,10 +1703,8 @@ case 'tiktoknowm': case 'ttnowm': case 'tiktok': case 'tt':
 if (!args[0]) {
         throw 'Uhm... URL-nya mana?';
     }
-
-    try {
         await reply('Tunggu sebentar kak, video sedang di download... server 1');
-
+    try {
         const tiktokData = await tryServer1(args[0]);
 
         if (!tiktokData) {
@@ -1768,38 +1724,11 @@ if (!args[0]) {
 
         if (videoURL || videoURLWatermark) {
             await conn.sendFile2(m.chat, videoURL, 'tiktok.mp4', `${done}\n\n${infonya_gan}`, m);
-            setTimeout(async () => {
-                //await conn.sendFile(m.chat, videoURLWatermark, 'tiktokwm.mp4', `*Ini Versi Watermark*\n\n${infonya_gan}`, m);
-                //await conn.sendFile(m.chat, `${tiktokData.music.play_url}`,'lagutt.mp3', 'ini lagunya',m);
-            }, 5000);
-        } else {
-            throw 'Tidak ada tautan video yang tersedia.';
-        }
-    } catch (error1) {
-        // jika server 1 gagal, gunakan server 2
-        try {
-            await m.reply('Tunggu sebentar kak, video sedang di download... server 2');
-            const tiktokData2 = await tryServer2(args[0]);
-
-            if (!tiktokData2) {
-                throw 'Gagal mendownload video!';
-            }
-
-            const videoURL2 = tiktokData2.video;
-            const audioURL2 = tiktokData2.audio;
-            const thumbnailURL = tiktokData2.thumbnail;
-
-            // Lakukan apa yang Anda perlukan dengan tiktokData2 dari Server 2 di sini
-            //await conn.sendFile(m.chat, thumbnailURL, 'thumbnail.jpg', 'Ini thumbnail videonya', m);
-            await conn.sendFile2(m.chat, videoURL2, 'tiktok2.mp4', 'Ini kak videonya dari Server 2', m);
-            //await conn.sendFile(m.chat, audioURL2, 'tiktok.mp3', 'Ini kak audionya', m);
-            m.reply("tiktok.ryzendesu.com");
-
-        } catch (error2) {
-            // Jika server kedua juga gagal, tangani error di sini
-            replyerror(`Error: ${error2}`);
-        }
     };
+              } catch (errornya) {
+            // Jika server kedua juga gagal, tangani error di sini
+            replyerror(`Error: ${errornya}`);
+            }
 break
 case 'tiktokmp3': case 'ttmp3': case 'ttaudio':
 if (!args[0]) {
